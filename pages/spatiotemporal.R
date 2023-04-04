3ecfa384e09c07f8d22a028b64d9f99b3d1ef36f
Jenpoer <jeistory1@gmail.com>
2023-04-04 22:54:07 +0800

    Spatiotemporal write-up

--- pages/spatiotemporal.R
+++ pages/spatiotemporal.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse, sf, tmap, shinycssloaders, shinyjs, sfdep, stpp, shinyalert, grDevices)
+pacman::p_load(tidyverse, sf, tmap, shinycssloaders, shinyjs, sfdep, stpp, shinyalert)
 source("data_manager.R")
 
 st_data_current <- hotspot_data[["aceh"]]
@@ -8,12 +8,12 @@ st_data_current <- hotspot_data[["aceh"]]
 spatiotemporal_ui <- function(mk_plot, mk_table, ehsa_map, stik_plot) {
   return(
     tabsetPanel(
-      tabPanel(title="Mann Kendall", 
-               st_mann_kendall_ui(mk_plot, mk_table)),
+      tabPanel(title="Space-Time K-Function", 
+               st_stik_ui(stik_plot)),
       tabPanel(title="Emerging Hotspot Analysis", 
                st_ehsa_ui(ehsa_map)),
-      tabPanel(title="Space-Time K-Function", 
-               st_stik_ui(stik_plot))
+      tabPanel(title="Mann Kendall", 
+               st_mann_kendall_ui(mk_plot, mk_table)),
     )
   )
 }
@@ -25,104 +25,160 @@ spatiotemporal_ui <- function(mk_plot, mk_table, ehsa_map, stik_plot) {
 st_mann_kendall_ui <- function(plot, table) {
   return(
     div(
-      sidebarPanel(
-        useShinyjs(),
-        selectInput(
-          "st_province",
-          "Province",
-          choices=names(hotspot_data)
-        ),
-        selectInput(
-          "st_city",
-          "City/Regency",
-          choices=c("", unique(st_data_current$city))
-        ),
-        disabled(
+      sidebarLayout(
+        sidebarPanel(
+          useShinyjs(),
           selectInput(
-            "st_district",
-            "District",
-            choices=c("", unique(st_data_current$district))
-          )
-        ),
-        disabled(
+            "st_province",
+            "Province",
+            choices=names(hotspot_data)
+          ),
           selectInput(
-            "st_sub_district",
-            "Sub-District",
-            choices=c("", unique(st_data_current$sub_district))
-          )
+            "st_city",
+            "City/Regency",
+            choices=c("", unique(st_data_current$city))
+          ),
+          disabled(
+            selectInput(
+              "st_district",
+              "District",
+              choices=c("", unique(st_data_current$district))
+            )
+          ),
+          disabled(
+            selectInput(
+              "st_sub_district",
+              "Sub-District",
+              choices=c("", unique(st_data_current$sub_district))
+            )
+          ),
+          numericInput(
+            "st_mk_nsim",
+            "Number of simulations",
+            value = 39
+          ),
+          selectInput(
+            "st_mk_contiguity_weights",
+            "Type of Contiguity Weights",
+            choices=c("Row Standardised", "Inverse Distance")
+          ),
+          selectInput(
+            "st_year",
+            "Year",
+            choices=c("2015",
+                      "2016",
+                      "2017",
+                      "2018",
+                      "2019")
+          ),
+          actionButton("st_submit", "Submit")
         ),
-        numericInput(
-          "st_mk_nsim",
-          "Number of simulations",
-          value = 39
+        mainPanel(
+          withSpinner(plotOutput(plot), type=1),
+          withSpinner(tableOutput(table), type=1),
         ),
-        selectInput(
-          "st_mk_contiguity_weights",
-          "Type of Contiguity Weights",
-          choices=c("Row Standardised", "Inverse Distance")
-        ),
-        selectInput(
-          "st_year",
-          "Year",
-          choices=c("2015",
-                    "2016",
-                    "2017",
-                    "2018",
-                    "2019")
-        ),
-        actionButton("st_submit", "Submit")
       ),
-      mainPanel(
-        withSpinner(plotOutput(plot), type=1),
-        withSpinner(tableOutput(table), type=1)
+      hr(),
+      div(
+        h4("Description"),
+        p("The Mann-Kendall test is a non-parametric statistical test used to detect whether a set of data values is monotonically increasing or decreasing, as well as the statistical significance of the trend. It is particularly useful when the data is not normally distributed or when the data has a significant amount of noise."),
+        p("In this section, you can perform Mann-Kendall tests on computed Getis-Ord Gi* statistics within a sub-district in a specified year. This will aid in closely examining the changes in the hot spots / cold spots over a year, and statistically confirm the presence of any trends."),
+        h4("How it works"),
+        p("For every test we want to perform, we define the hypothesis as such:"),
+        tags$ul(
+          tags$li("H0: There is no monotonic trend in the series."),
+          tags$li("H1:  A trend exists. This trend can be positive, negative, or non-null.")
+        ),
+        p("The Mann Kendall test is performed using the Kendall package."),
+        p("After running the test and observing a p-value, you can choose whether to reject the null hypothesis or not based on your own chosen significance level."),
+        h4("Intepretation"),
+        p("After running the function with all your selected parameters, you will see a time series plot of the change in Gi* values in the sub-district over the chosen year."),
+        p("Below it, you'll see the results of the Mann-Kendall test in a table. The values in the table are:"),
+        tags$ul(
+          tags$li("tau: Kendall's tau statistic"),
+          tags$li("sl: Two-sided p-value"),
+          tags$li("S: Kendall Score"),
+          tags$li("D: Denominator (tau = S / D)"),
+          tags$li("varS: Variance of S")
+        ),
+        p("Kendall's tau statistic varies between -1 and 1. A positive number indicates an upwards trend, whereas a negative number indicates a downwards trend."),
+        p("The two-sided p-value will help you determine whether or not it is appropriate to reject the null hypothesis.")
       )
     )
+    
   )
 }
 
 st_ehsa_ui <- function(map) {
   return(
     div(
-      sidebarPanel(
-        useShinyjs(),
-        selectInput(
-          "st_province_2",
-          "Province",
-          choices=names(hotspot_data)
+      sidebarLayout(
+        sidebarPanel(
+          useShinyjs(),
+          selectInput(
+            "st_province_2",
+            "Province",
+            choices=names(hotspot_data)
+          ),
+          selectInput(
+            "st_city_2",
+            "City/Regency",
+            choices=c("", unique(st_data_current$city))
+          ),
+          numericInput(
+            "st_ehsa_k",
+            "Number of time lags",
+            value = 1
+          ),
+          numericInput(
+            "st_ehsa_nsim",
+            "Number of simulations",
+            value = 99
+          ),
+          numericInput(
+            "st_ehsa_threshold",
+            "Significance threshold",
+            value = 0.01
+          ),
+          selectInput(
+            "st_year_2",
+            "Year",
+            choices=c("2015",
+                      "2016",
+                      "2017",
+                      "2018",
+                      "2019")
+          ),
+          actionButton("st_submit_2", "Submit")
         ),
-        selectInput(
-          "st_city_2",
-          "City/Regency",
-          choices=c("", unique(st_data_current$city))
-        ),
-        numericInput(
-          "st_ehsa_k",
-          "Number of time lags",
-          value = 1
-        ),
-        numericInput(
-          "st_ehsa_nsim",
-          "Number of simulations",
-          value = 99
-        ),
-        numericInput(
-          "st_ehsa_threshold",
-          "Significance threshold",
-          value = 0.01
-        ),
-        selectInput(
-          "st_year_2",
-          "Year",
-          choices=c("2015",
-                    "2016",
-                    "2017",
-                    "2018",
-                    "2019")
-        ),
-        actionButton("st_submit_2", "Submit")
+        mainPanel(
+          withSpinner(plotOutput(map), type=1)
+        )
       ),
-      mainPanel(
-        withSpinner(plotOutput(map), type=1)
+      hr(),
+      div(
+        h4("Description"),
+        p("Emerging Hot Spot Analysis (EHSA) is a spatiotemporal analysis technique used to identify locations where the density of a specific event is increasing rapidly over time."),
+        p("Note that 'hot spot' in this context does not refer to fire hotspots, but spatial clusters with significantly high number of fire hotspots."),
+        p("In this section, you can perform EHSA on an areal basis using aggregated count of fire hotspots within a province or a city. I.e. the feature we are looking at is the count of fire hotspots in a region."),
+        h4("How it works"),
+        p("The EHSA is performed using sfdep's emerging_hotspots_analysis(), which combines the Getis-Ord Gi* statistic with Mann-Kendall test."),
+        p("The Gi* statistic calculates the local spatial autocorrelation for each feature in a dataset by comparing the feature's value with the values of neighboring features."),
+        p("You are able to adjust the number of time lags, number of simulations for Gi* calculation, and significance threshold."),
+        h4("Intepretation"),
+        p("After running the function with all your selected parameters, you will see a choropleth map. The different colors of the sub-districts indicate the kind of trend is detected with statistical significance."),
+        p("The possible patterns are:"),
+        tags$ul(
+          tags$li("No pattern detected"),
+          tags$li("New Hot Spot / Cold Spot: A location that is a statistically significant hot spot (or cold spot) at the final time step without having been significant prior."),
+          tags$li("Consecutive Hot Spot / Cold Spot: A location with a single uninterrupted run of at least 2 statistically significant hot spot (or cold spot) bins in the final time-step intervals."),
+          tags$li("Intensifying Hot Spot / Cold Spot: A location that has been a statistically significant hot spot (or cold spot) for 90% of the time and the intensity of clustering increases."),
+          tags$li("Persistent Hot Spot / Cold Spot: A location that has been a statistically significant hot spot (or cold spot) for 90% of the time-step intervals with no trend in the clustering intensity."),
+          tags$li("Diminishing Hot Spot / Cold Spot: A location that has been a statistically significant hot spot (or cold spot) for 90% of the time-step intervals and the intensity of clustering decreases."),
+          tags$li("Sporadic Hot Spot / Cold Spot: A statistically significant hot spot (or cold spot) for the final time step interval with a history of being an on-off hot spot (or cold spot). No history of being a statistically significant cold spot (or hot spot)."),
+          tags$li("Oscillating Hot Spot / Cold Spot: A statistically significant hot spot (or cold spot) for the final time step interval with a history of being an on-off hot spot (or cold spot). Has a history of being a statistically significant cold spot (or hot spot)."),
+          tags$li("Historical Hot Spot / Cold Spot: The most recent time period is not hot (or cold), but at least 90% of time step intervals have been statistically significant hot spots (or cold spots).")
+        ),
       )
     )
   )
@@ -131,76 +187,91 @@ st_ehsa_ui <- function(map) {
 st_stik_ui <- function(plot_name) {
   return(
     div(
-      sidebarPanel(
-        useShinyjs(),
-        selectInput(
-          "st_province_3",
-          "Province",
-          choices=names(hotspot_data)
-        ),
-        selectInput(
-          "st_city_3",
-          "City/Regency",
-          choices=c("", unique(st_data_current$city))
-        ),
-        disabled(
-          selectInput(
-            "st_district_3",
-            "District",
-            choices=c("", unique(st_data_current$district))
-          )
-        ),
-        disabled(
-          selectInput(
-            "st_sub_district_3",
-            "Sub-District",
-            choices=c("", unique(st_data_current$sub_district))
-          )
-        ),
-        selectInput(
-          "st_year_3",
-          "Year",
-          choices=c("2015",
-                    "2016",
-                    "2017",
-                    "2018",
-                    "2019")
-        ),
-        actionButton("st_submit_3", "Submit")
-      ),
-      mainPanel(
-        withSpinner(plotOutput(plot_name), type=1),
-        fluidRow(
+      sidebarLayout(
+        sidebarPanel(
           useShinyjs(),
-          column(4, 
-                 selectInput(
-                  "st_stik_plot_type",
-                  "Plot Type",
-                  choices=c("Contour",
-                            "Image",
-                            "Perspective")
-                )
+          selectInput(
+            "st_province_3",
+            "Province",
+            choices=names(hotspot_data)
           ),
-          column(4, 
-                 disabled(sliderInput(
-                   "st_persp_theta",
-                   "Azimuthal Angle",
-                   min = 0,
-                   max = 360,
-                   value = 0
-                 ))
+          selectInput(
+            "st_city_3",
+            "City/Regency",
+            choices=c("", unique(st_data_current$city))
           ),
-          column(4, 
-                 disabled(sliderInput(
-                   "st_persp_phi",
-                   "Colatitude",
-                   min = 0,
-                   max = 360,
-                   value = 15
-                 ))
-          ))
+          disabled(
+            selectInput(
+              "st_district_3",
+              "District",
+              choices=c("", unique(st_data_current$district))
+            )
+          ),
+          disabled(
+            selectInput(
+              "st_sub_district_3",
+              "Sub-District",
+              choices=c("", unique(st_data_current$sub_district))
+            )
+          ),
+          selectInput(
+            "st_year_3",
+            "Year",
+            choices=c("2015",
+                      "2016",
+                      "2017",
+                      "2018",
+                      "2019")
+          ),
+          actionButton("st_submit_3", "Submit")
+        ),
+        mainPanel(
+          withSpinner(plotOutput(plot_name), type=1),
+          fluidRow(
+            useShinyjs(),
+            column(4, 
+                   selectInput(
+                     "st_stik_plot_type",
+                     "Plot Type",
+                     choices=c("Contour",
+                               "Image",
+                               "Perspective")
+                   )
+            ),
+            column(4, 
+                   disabled(sliderInput(
+                     "st_persp_theta",
+                     "Azimuthal Angle",
+                     min = 0,
+                     max = 360,
+                     value = 0
+                   ))
+            ),
+            column(4, 
+                   disabled(sliderInput(
+                     "st_persp_phi",
+                     "Colatitude",
+                     min = 0,
+                     max = 360,
+                     value = 15
+                   ))
+            ))
+        )
+      ),
+      hr(),
+      div(
+        h4("Description"),
+        p("The space-time K-function is a statistical tool used in spatiotemporal analysis to study the clustering or dispersion of point events in both space and time. It extends the classical spatial Ripley's K-function to incorporate the time dimension and allows for the analysis of spatiotemporal point patterns."),
+        p("In the inhomogenous case, it takes into account the spatial and temporal variation in the intensity of the point process under study. Meanwhile, the homogenous case assumes that the underlying process generating the point pattern is constant in space and time."),
+        p("It provides a powerful tool for understanding the complex interactions between spatial and temporal processes and for identifying patterns and trends that might otherwise be missed."),
+        h4("How it works"),
+        p("You can input a specific sub-district to look for spatiotemporal autocorrelation throughout a specific year. The space-time K-function will then be calculated using stpp's STIK function for the specified parameters."),
+        h4("Intepretation"),
+        p("After running the function with all your selected parameters, you will see either a contour plot, an image plot, or a perspective plot depending on your selection of Plot Type. The axes indicate distances in meters and times in days. The plot displays values for K - 2 pi u^2 v."),
+        p("Hence, a positive value implies K > 2 pi u^2 v, indicating clustering. Conversely, a negative value implies K < 2 pi u^2 v, indicating regularity.")
       )
     )
+    
   )
 }
 
